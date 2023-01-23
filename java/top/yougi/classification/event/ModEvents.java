b190fd130061879b3432c35d824be76ca8e012c7
Westlifers <2585687459@qq.com>
2023-01-23 20:31:00 +0800

    放置导致箱子合并时自动调整capability

--- java/top/yougi/classification/event/ModEvents.java
+++ java/top/yougi/classification/event/ModEvents.java
@@ -1,12 +1,19 @@
 package top.yougi.classification.event;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.ChestType;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
@@ -33,6 +40,61 @@ public class ModEvents {
                         new ChestCapabilityProvider());
             }
         }
+    }
+
+    // 玩家放置箱子时调整capability
+    @SubscribeEvent
+    public static void onPlayerPlaceChest(BlockEvent.EntityPlaceEvent event) {
+        BlockPos pos = event.getPos();
+        Level level = event.getEntity().getLevel();
+        BlockState state = event.getPlacedBlock();
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+
+       if (state.getBlock() instanceof ChestBlock) {
+           ChestType type = state.getValue(BlockStateProperties.CHEST_TYPE);
+           Direction facing = state.getValue(BlockStateProperties.HORIZONTAL_FACING);
+
+           BlockPos nearPos;
+           if (!type.equals(ChestType.SINGLE)) {
+               if (type.equals(ChestType.LEFT)) {
+                   if (facing.equals(Direction.NORTH)) {
+                       nearPos = pos.offset(1, 0, 0);
+                   }
+                   else if (facing.equals(Direction.SOUTH)) {
+                       nearPos = pos.offset(-1, 0, 0);
+                   }
+                   else if (facing.equals(Direction.EAST)) {
+                       nearPos = pos.offset(0, 0, 1);
+                   }
+                   else {
+                       nearPos = pos.offset(0, 0, -1);
+                   }
+               }
+               else {
+                   if (facing.equals(Direction.NORTH)) {
+                       nearPos = pos.offset(-1, 0, 0);
+                   }
+                   else if (facing.equals(Direction.SOUTH)) {
+                       nearPos = pos.offset(1, 0, 0);
+                   }
+                   else if (facing.equals(Direction.EAST)) {
+                       nearPos = pos.offset(0, 0, -1);
+                   }
+                   else {
+                       nearPos = pos.offset(0, 0, 1);
+                   }
+               }
+
+               BlockEntity nearBlockEntity = level.getBlockEntity(nearPos);
+               if (nearBlockEntity.getCapability(ChestCapabilityProvider.CHEST_CAPABILITY).isPresent()) {
+                   nearBlockEntity.getCapability(ChestCapabilityProvider.CHEST_CAPABILITY).ifPresent(cap -> {
+                       blockEntity.getCapability(ChestCapabilityProvider.CHEST_CAPABILITY).ifPresent(cap_ -> {
+                           cap_.setClassName(cap.getClassName());
+                       });
+                   });
+               }
+           }
+       }
     }
 
     // 为世界绑定LevelCapability
